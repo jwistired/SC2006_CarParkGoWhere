ac9b67569f96adf0da00d6fc5a61c06798b4ac3c
Dlhwai <106801652+Dlhwai@users.noreply.github.com>
2024-10-09 21:51:14 +0800

    added reset password OTP incorrect error message and fixed logic. Created successResetPassword redirect and page.

--- SC2006 Project/BoundaryClasses/ResetPassword.js
+++ SC2006 Project/BoundaryClasses/ResetPassword.js
@@ -1,23 +1,32 @@
 const passport = require('passport')
 const express = require('express')
 const router = express.Router()
-const otp = require('otp-generator')
 const bcrypt = require('bcrypt')
+const {checkAuthenticated, checkNotAuthenticated} = require('./Authenticator.js')
 
-router.get('/', (req, res) => {
+
+router.get('/',checkAuthenticated ,(req, res) => {
     res.render('resetPassword.ejs', { email: req.user.email })
+})
+
+router.get('/successResetPassword', checkNotAuthenticated, (req,res) => {
+    res.render('successResetPassword.ejs')
 })
 
 router.post('/', async (req, res) => {
     var userOTP = req.body.OTP
-    console.log(req.body.password)
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
 
     if(userOTP == req.session.generatedOTP){
         req.user.password = hashedPassword
-        res.redirect('/login')
+        req.logOut(function(err) {
+            if (err) { 
+                return next(err) 
+            }
+            res.redirect('/resetPassword/successResetPassword')
+        })
     } else {
-        res.status(400).send("Invalid OTP")
+        req.flash('error', 'OTP incorrect')
         res.redirect('/resetPassword')
     }
 })
