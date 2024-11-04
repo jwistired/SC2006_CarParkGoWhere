f580d991b6bf32afa6c141145ad4f121354f287a
Dlhwai <106801652+Dlhwai@users.noreply.github.com>
2024-11-04 21:39:45 +0800

    Added confirm password, password requirement for reset password.

--- SC2006 Project/BoundaryClasses/ResetPassword.js
+++ SC2006 Project/BoundaryClasses/ResetPassword.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcrypt');
 const { getByEmail } = require('./Database')
 const otpExpiryTime = 5 * 60 * 1000; // Time for OTP expiry, adjust first parameter
 const { checkAuthenticated, checkNotAuthenticated, terminateAuthentication} = require('./Authenticator.js');
+const passwordCriteria = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,20}$/;
+
 
 router.get('/', checkAuthenticated, (req, res) => {
     res.render('resetPassword.ejs', { email: req.user.email });
@@ -17,6 +19,8 @@ router.get('/successResetPassword', checkNotAuthenticated, (req, res) => {
 router.post('/', async (req, res, next) => {
     var userOTP = req.body.OTP;
     var otpGeneratedAt = req.session.OTPGeneratedAt;
+    var confirmPassword = req.body.confirmPassword;
+    var password = req.body.password
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     
     // Check if OTP is expired
@@ -30,6 +34,16 @@ router.post('/', async (req, res, next) => {
     
     // Check if OTP is correct
     if (userOTP == req.session.generatedOTP) {
+        // Password validation
+        if (!passwordCriteria.test(password)) {
+            req.flash('error', "Password must be between 8 and 20 characters, include at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character." );
+            return res.redirect('/resetPassword')
+        }
+        // Check that passwords match
+        if (password !== confirmPassword) {
+            req.flash('error', "Passwords do not match. Please make sure both password fields are identical.")
+            return res.redirect('/resetPassword')
+        }
         //console.log(req.session.email)
         // inititate user by checking email 
         const user = await getByEmail(req.session.email)
