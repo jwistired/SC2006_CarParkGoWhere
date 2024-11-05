c8479918f2ce79fe52e969efe3adbab180abce34
Dlhwai <106801652+Dlhwai@users.noreply.github.com>
2024-11-05 20:41:53 +0800

    fixed forgetpassword.ejs, moved schemas to respective models

--- SC2006 Project/models/CarPark.js
+++ SC2006 Project/models/CarPark.js
@@ -1,15 +1,36 @@
 // models/CarPark.js
 const mongoose = require('mongoose');
 
-const carParkSchema = new mongoose.Schema({
-  location: String,
-  totalSpots: Number,
-  availableSpots: Number
-});
+const historySchema = new mongoose.Schema({
+  email: {
+      type: String,
+      required: true
+  },
+  car_park_no: {
+      type: String,
+      required: true
+  },
+  address: {
+      type: String,
+      required: true
+  },
+  x_cord: {
+      type: Number,
+      require: true
+  },
+  y_cord: {
+      type: Number,
+      require: true
+  },
+  time: { 
+      type: Date, 
+      default: Date.now
+  }
+})
 
-carParkSchema.statics.findNearby = async function (location) {
+/*carParkSchema.statics.findNearby = async function (location) {
   // Logic to find car parks near a given location
   return this.find({ location: { $near: location } });
-};
+};*/
 
-module.exports = mongoose.model('CarPark', carParkSchema);
+module.exports = historySchema
